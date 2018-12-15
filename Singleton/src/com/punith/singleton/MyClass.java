08a34f4dfe675f3d02c0ab3030dc89550b4c39b0
Raaj <punithraj14@gmail.com>
2018-12-15 16:33:31 +0530

    new Project

---
+++ Singleton/src/com/punith/singleton/MyClass.java
@@ -0,0 +1,7 @@
+package com.punith.singleton;
+import com.punith.singleton.*;
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println(Singleton.getInstances().getMyname());
+    }
+}
--- Singleton/src/com/punith/singleton/Singleton.java
+++ Singleton/src/com/punith/singleton/Singleton.java
@@ -1,4 +1,29 @@
 package com.punith.singleton;
 
 public class Singleton {
+
+    static Singleton singleton = null;
+
+    public String getMyname() {
+        return Myname;
+    }
+
+    public void setMyname(String myname) {
+        Myname = myname;
+    }
+
+    private String Myname = "punith";
+    public static Singleton  getInstances() {
+        if (singleton == null){
+            singleton = new Singleton();
+         }
+        return singleton;
+    }
+    public Singleton getSingleton() {
+        return singleton;
+    }
+
+    public void setSingleton(Singleton singleton) {
+        this.singleton = singleton;
+    }
 }
